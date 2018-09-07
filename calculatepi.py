64b36e518195d74ef70b900ec9ef4a64b10e162d
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-09-07 12:05:56 -0400

    Updated from Brython Server: 9/7/2018 12:05:56 PM

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Katie Naughton
+Credit: I worked alone. 
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
