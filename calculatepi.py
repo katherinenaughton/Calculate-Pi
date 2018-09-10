5f8b3ef4f8a0f919a2d0d72f20cdd412e4b81727
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-09-10 12:10:07 -0400

    Updated from Brython Server: 9/10/2018 12:10:06 PM

--- calculatepi.py
+++ calculatepi.py
@@ -24,9 +24,8 @@ Note: remember that the printed value of pi will be an estimate!
 
 print('I will estimate pi.')
 n=int(input('How many terms should I use? '))
-decimals=float(input('How many decimal places should I use in the result? '))
+decimals=int(input('How many decimal places should I use in the result? '))
 import math
 pi=4sum([(-1.0**k/math.factorial(k) for k in range(0,n)]))
-
 print('(The approximate value of pi is {0})'.format(round( pi, decimals)))
 
